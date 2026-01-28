8c5730c75b0fed91d1e67809d8d301bf62020c2d
vibhav2601 <vibhav2601@gatech.edu>
2026-01-28 18:11:56 +0530

    fix the landing routing

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/priyanshineelesh/">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/explore" element={<Explore />} />
